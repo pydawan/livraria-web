0fce34c76ca558ee01d892391f51e8d7ff8a57f3
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2019-01-09 00:52:07 -0200

    Definida a classe Livro. O erro de compilação será resolvido porém o teste test() irá falhar pois não foi implementado.

---
+++ src/main/java/br/com/livrariaweb/model/Livro.java
@@ -0,0 +1,12 @@
+package br.com.livrariaweb.model;
+
+/**
+ * Abstração de um livro no contexto do sistema.
+ * 
+ * @author thiago-amm
+ * @version v1.0.0 09/01/2019
+ * @since v1.0.0
+ */
+public class Livro {
+    
+}
