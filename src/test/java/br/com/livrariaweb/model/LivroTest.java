cd6045b19a1d1ad64b9f716fcf85c86374a3ec1b
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2019-01-09 00:46:08 -0200

    Criado teste unitário para a classe Livro.

---
+++ src/test/java/br/com/livrariaweb/model/LivroTest.java
@@ -0,0 +1,19 @@
+package br.com.livrariaweb.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author thiago-amm
+ * @version v1.0.0 09/01/2019
+ * @since v1.0.0
+ */
+public class LivroTest {
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+}
