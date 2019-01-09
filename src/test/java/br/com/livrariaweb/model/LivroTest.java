34b23386fae1a80c13df558ff2961cb172f7cc0e
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2019-01-09 01:07:40 -0200

    Armazenada a constante "J.R.R Tolkien" no atributo (ainda não definido) autor do objeto livro da classe Livro.

--- src/test/java/br/com/livrariaweb/model/LivroTest.java
+++ src/test/java/br/com/livrariaweb/model/LivroTest.java
@@ -17,6 +17,7 @@ public class LivroTest {
         Assert.assertTrue("FALHA: \"O id do livro não é igual a 1 como se esperava!\"", 1 == livro.id);
         livro.titulo = "O Senhor dos Anéis";
         Assert.assertEquals("FALHA: \"O título do livro não é igual a O Senhor dos Anéis como se esperava!\"", "O Senhor dos Anéis", livro.titulo);
+        livro.autor ="J.R.R Tolkien";
     }
     
 }
