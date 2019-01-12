f1ff3cdbfd0945d64f73bc3b71f6bb17b5580f8a
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2019-01-12 07:49:35 -0200

    Atualizada classe de testes unitários da classe Livro para produzir log quando ocorrer um java.lang.AssertionError.

--- src/test/java/br/com/livrariaweb/model/LivroTest.java
+++ src/test/java/br/com/livrariaweb/model/LivroTest.java
@@ -1,5 +1,8 @@
 package br.com.livrariaweb.model;
 
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,16 +13,23 @@ import org.junit.Test;
  */
 public class LivroTest {
     
+    private static final Logger logger = Logger.getLogger(LivroTest.class);
+    
     @Test
     public void test() {
-        Livro livro = new Livro();
-        livro.setId(1);
-        Assert.assertTrue("FALHA: \"O id do livro não é igual a 1 como se esperava!\"", 1 == livro.getId() );
-        livro.setTitulo("O Senhor dos Anéis");
-        Assert.assertEquals("FALHA: \"O título do livro não é igual a O Senhor dos Anéis como se esperava!\"", "O Senhor dos Anéis", livro.getTitulo() );
-        livro.setAutor("J.R.R Tolkien");
-        Assert.assertEquals("FALHA: \"O autor do livro não é igual a J.R.R Tolkien como se esperava!\"", "J.R.R Tolkien", livro.getAutor() );
-        System.out.println(livro);
+        try {
+            Livro livro = new Livro();
+            livro.setId(2);
+            Assert.assertTrue("FALHA: \"O id do livro não é igual a 1 como se esperava!\"", 1 == livro.getId() );
+            livro.setTitulo("O Senhor dos Anéis");
+            Assert.assertEquals("FALHA: \"O título do livro não é igual a O Senhor dos Anéis como se esperava!\"", "O Senhor dos Anéis", livro.getTitulo() );
+            livro.setAutor("J.R.R Tolkien");
+            Assert.assertEquals("FALHA: \"O autor do livro não é igual a J.R.R Tolkien como se esperava!\"", "J.R.R Tolkien", livro.getAutor() );
+            System.out.println(livro);
+        } catch (AssertionError e) {
+            logger.error(e);
+            fail(e.getMessage());
+        }
     }
     
 }
