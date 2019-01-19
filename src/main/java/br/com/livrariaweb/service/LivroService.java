170a59aff61b2bc26a68301477c11445dc6378fb
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2019-01-19 18:34:58 -0200

    Adicionada classe de serviço para a classe Livro.

---
+++ src/main/java/br/com/livrariaweb/service/LivroService.java
@@ -0,0 +1,38 @@
+package br.com.livrariaweb.service;
+
+import java.util.List;
+
+import br.com.livrariaweb.dao.LivroDAO;
+import br.com.livrariaweb.model.Livro;
+
+/**
+ * @author thiago-amm
+ * @version v1.0.0 19/01/2019
+ * @since v1.0.0
+ */
+public class LivroService {
+    
+    private LivroDAO livroDAO;
+    
+    public LivroService(LivroDAO livroDAO) {
+        this.livroDAO = livroDAO;
+    }
+    
+    public LivroService() {
+        this(new LivroDAO());
+    }
+    
+    public static LivroService of(LivroDAO livroDAO) {
+        return new LivroService(livroDAO);
+    }
+    
+    public static LivroService of() {
+        return new LivroService();
+    }
+    
+    public List<Livro> listarLivros() {
+        List<Livro> livros = livroDAO.selectAll();
+        return livros;
+    }
+    
+}
